addfd025dee00df04e6bde32e2a2856512416497
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-09-08 11:00:29 +0300

    Provide custom imageloaders for cache on each platformes

--- composeApp/src/iosMain/kotlin/com/rcl/nextshiki/App.ios.kt
+++ composeApp/src/iosMain/kotlin/com/rcl/nextshiki/App.ios.kt
@@ -1,9 +1,39 @@
 package com.rcl.nextshiki
 
+import com.seiko.imageloader.ImageLoader
+import com.seiko.imageloader.component.setupDefaultComponents
+import com.seiko.imageloader.defaultImageResultMemoryCache
+import okio.Path.Companion.toPath
 import platform.Foundation.NSURL
 import platform.UIKit.UIApplication
 
 internal actual fun openUrl(url: String?) {
     val nsUrl = url?.let { NSURL.URLWithString(it) } ?: return
     UIApplication.sharedApplication.openURL(nsUrl)
+}
+
+internal actual fun generateImageLoader(): ImageLoader {
+    return ImageLoader {
+        components {
+            setupDefaultComponents()
+        }
+        interceptor {
+            defaultImageResultMemoryCache()
+            memoryCacheConfig {
+                maxSizeBytes(32 * 1024 * 1024)
+            }
+            diskCacheConfig {
+                directory(getCacheDir().toPath().resolve("image_cache"))
+                maxSizeBytes(512L * 1024 * 1024)
+            }
+        }
+    }
+}
+
+private fun getCacheDir(): String {
+    return NSSearchPathForDirectoriesInDomains(
+        NSCachesDirectory,
+        NSUserDomainMask,
+        true,
+    ).first() as String
 }
