86a548c47d1d04e9f9d060e289e1e521c9c0a511
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-09-13 13:46:21 +0300

    Redesign card to android's current version

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainScreen.kt
@@ -1,23 +1,27 @@
 package com.rcl.nextshiki.screens.main
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import com.rcl.nextshiki.MR
-import com.rcl.nextshiki.elements.CalendarCardPreview
+import com.rcl.nextshiki.elements.CalendarCard
 import com.rcl.nextshiki.getString
 
 @OptIn(ExperimentalMaterial3Api::class)
 object MainScreen : Screen {
+    @ExperimentalAnimationApi
     @Composable
     override fun Content() {
         val vm = remember { MainViewModel() }
+
         LaunchedEffect(Unit){
             vm.init(this)
         }
@@ -38,15 +42,24 @@ object MainScreen : Screen {
         ) {
             Box(modifier = Modifier.padding(it)){
                 Column {
-                    AnimatedVisibility(!vm.calendarList.isEmpty()){
-
-                        Card(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp)
-                                .height(220.dp),
-                        ) {
-                            CalendarCardPreview(vm)
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                            .height(220.dp),
+                    ) {
+                        AnimatedContent(!vm.calendarList.isEmpty()){
+                            if(it){
+                                CalendarCard(vm.previewName ,vm.nearTitle.anime!!.image!!.preview!!, vm.nearTitle.nextEpisodeAt!!)
+                            }
+                            else{
+                                Box(
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentAlignment = Alignment.Center
+                                ){
+                                    CircularProgressIndicator()
+                                }
+                            }
                         }
                     }
                 }
--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.rcl.nextshiki.screens.main
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.text.intl.Locale
 import com.rcl.nextshiki.ViewModel
 import com.rcl.nextshiki.di.ktor.KtorRepository
 import com.rcl.nextshiki.koin
@@ -8,19 +9,23 @@ import com.rcl.nextshiki.models.calendar.CalendarModel
 import com.rcl.nextshiki.screens.main.MainScreenObject.calendar
 import com.rcl.nextshiki.screens.main.MainScreenObject.currModel
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.launch
 
 
 class MainViewModel : ViewModel() {
     val calendarList = mutableStateListOf<CalendarModel>()
     lateinit var nearTitle: CalendarModel
+    lateinit var previewName: String
     override fun init(viewModelScope: CoroutineScope) {
         super.init(viewModelScope)
 
         if (calendar.isNotEmpty()) {
             calendarList.addAll(calendar)
             nearTitle = currModel
+            when(Locale.current.language){
+                "ru" -> previewName = nearTitle.anime!!.russian!!
+                "en" -> previewName = nearTitle.anime!!.name!!
+            }
             return
         }
         viewModelScope.launch {
@@ -28,6 +33,11 @@ class MainViewModel : ViewModel() {
             currModel = calendar[0]
             calendarList.addAll(calendar)
             nearTitle = currModel
+
+            when(Locale.current.language){
+                "ru" -> previewName = nearTitle.anime!!.russian!!
+                "en" -> previewName = nearTitle.anime!!.name!!
+            }
         }
     }
 }
