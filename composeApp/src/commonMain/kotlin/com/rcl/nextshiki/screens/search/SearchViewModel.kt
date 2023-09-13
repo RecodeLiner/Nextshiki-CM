6d52050efb17a42a70e039ff9bf67db7ed4bfc48
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-09-14 00:30:18 +0300

    Setup SearchViewModel

---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/search/SearchViewModel.kt
@@ -0,0 +1,11 @@
+package com.rcl.nextshiki.screens.search
+
+import com.rcl.nextshiki.ViewModel
+import kotlinx.coroutines.CoroutineScope
+
+class SearchViewModel: ViewModel() {
+    override fun init(viewModelScope: CoroutineScope) {
+        super.init(viewModelScope)
+
+    }
+}
