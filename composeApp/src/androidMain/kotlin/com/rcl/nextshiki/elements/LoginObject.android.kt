f90d0a2ea32ef8d0c8905123ed4ca2e5f90d9ef2
Lucker <Samir-ramazanov-1995@mail.ru>
2023-09-16 23:42:35 +0300

    rewrite profile screen #2

---
+++ composeApp/src/androidMain/kotlin/com/rcl/nextshiki/elements/LoginObject.android.kt
@@ -0,0 +1,8 @@
+package com.rcl.nextshiki.elements
+
+import androidx.compose.runtime.Composable
+import com.rcl.nextshiki.screens.profile.ProfileViewModel
+
+@Composable
+internal actual fun LoginObject(vm: ProfileViewModel) {
+}
