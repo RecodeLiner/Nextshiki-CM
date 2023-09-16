d4abc4358611077f01fa11f690fb06cdc501f115
Lucker <Samir-ramazanov-1995@mail.ru>
2023-09-16 23:38:26 +0300

    add token update for MainViewModel

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainViewModel.kt
@@ -4,9 +4,14 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.text.intl.Locale
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.coroutineScope
+import com.rcl.nextshiki.di.ktor.KtorModel
 import com.rcl.nextshiki.di.ktor.KtorRepository
+import com.rcl.nextshiki.getToken
 import com.rcl.nextshiki.koin
 import com.rcl.nextshiki.models.calendar.CalendarModel
+import com.rcl.nextshiki.settings
+import com.russhwolf.settings.contains
+import com.russhwolf.settings.set
 import kotlinx.coroutines.launch
 
 
@@ -16,6 +21,15 @@ class MainViewModel : ScreenModel {
     lateinit var previewName: String
     init{
         coroutineScope.launch {
+            if (settings.contains("refCode")){
+                val token = getToken(isFirst = false, code = settings.getString("refCode", ""))
+                KtorModel.token.value = token.accessToken!!
+                settings["refCode"] = token.refreshToken!!
+
+                val obj = koin.get<KtorRepository>().getCurrentUser()
+                settings["id"] = obj!!.id!!
+            }
+
             calendarList.addAll(koin.get<KtorRepository>().getCalendar())
             nearTitle = calendarList[0]
 
