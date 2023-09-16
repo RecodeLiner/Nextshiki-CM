7df4163a31d52c073a27167440ad80c90c21925b
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-09-16 11:05:31 +0300

    Replace string in routes with stringResources for translate

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/Routes.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/Routes.kt
@@ -2,9 +2,10 @@ package com.rcl.nextshiki.screens
 
 import androidx.compose.ui.graphics.vector.ImageVector
 import cafe.adriel.voyager.core.screen.Screen
+import dev.icerock.moko.resources.StringResource
 
 data class Routes(
-    val name: String,
+    val name: StringResource,
     val screen: Screen,
     val outlinedIcon: ImageVector,
     val filledIcon: ImageVector,
