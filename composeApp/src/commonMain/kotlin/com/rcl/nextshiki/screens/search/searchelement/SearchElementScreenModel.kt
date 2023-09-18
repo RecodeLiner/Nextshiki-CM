1874a0b1cbbd6bc99ef77d98bfde253f557e139f
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-09-19 01:25:12 +0300

    Update search element screen and simple adaptive ui

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/search/searchelement/SearchElementScreenModel.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/search/searchelement/SearchElementScreenModel.kt
@@ -1,24 +1,41 @@
 package com.rcl.nextshiki.screens.search.searchelement
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.intl.Locale
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.coroutineScope
 import com.rcl.nextshiki.di.ktor.KtorRepository
 import com.rcl.nextshiki.koin
+import com.rcl.nextshiki.models.moe.ResultModel
 import com.rcl.nextshiki.models.searchobject.ObjById
+import io.github.aakira.napier.Napier
 import kotlinx.coroutines.launch
 
-class SearchElementScreenModel(type: String, id: String) : ScreenModel {
-    val CurrObj = mutableStateOf<ObjById?>(null)
+class SearchElementScreenModel(val type: String, val id: String) : ScreenModel {
+    val currObj = mutableStateOf<ObjById?>(null)
     val titleName = mutableStateOf("")
+    val listForAnimeContent = mutableStateListOf<ResultModel>()
     init {
         coroutineScope.launch {
-            CurrObj.value = koin.get<KtorRepository>().getObjectById(type = type, id = id)
+            currObj.value = koin.get<KtorRepository>().getObjectById(type = type, id = id)
             when (Locale.current.language) {
-                "ru" -> titleName.value = CurrObj.value!!.russian!!
-                "en" -> titleName.value = CurrObj.value!!.name!!
+                "ru" -> titleName.value = currObj.value!!.russian!!
+                "en" -> titleName.value = currObj.value!!.name!!
             }
+        }
+    }
+
+    fun copyContent(clipboard: ClipboardManager, text: String) {
+        clipboard.setText(AnnotatedString(text))
+    }
+    fun getLinks() {
+        coroutineScope.launch {
+            val list = koin.get<KtorRepository>().getVideoLinks(currObj.value!!.id!!.toString()).result
+            listForAnimeContent.addAll(list)
+            Napier.i(list.toString())
         }
     }
 }
