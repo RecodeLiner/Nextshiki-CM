ed3bac2a53f0b5d59618f922ff4047e3ae7d6dea
Lucker <Samir-ramazanov-1995@mail.ru>
2023-09-18 12:29:24 +0300

    Update to edge-to-edge

--- composeApp/src/androidMain/kotlin/com/rcl/nextshiki/MatTheme.kt
+++ composeApp/src/androidMain/kotlin/com/rcl/nextshiki/MatTheme.kt
@@ -6,11 +6,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rcl.nextshiki.theme.Theme.AppShapes
 import com.rcl.nextshiki.theme.Theme.getTypography
 import com.rcl.nextshiki.theme.ThemeColors.darkColor
@@ -31,18 +27,6 @@ object MatTheme {
             }
 
             else -> if (darkTheme) darkColor else lightColor
-        }
-
-        val systemUiController = rememberSystemUiController()
-        val view = LocalView.current
-
-        if (!view.isInEditMode) {
-            SideEffect {
-                systemUiController.setSystemBarsColor(
-                    color = Color.Transparent,
-                    darkIcons = !darkTheme
-                )
-            }
         }
 
         MaterialTheme(
