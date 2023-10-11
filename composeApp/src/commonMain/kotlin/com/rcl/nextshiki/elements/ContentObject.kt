d111e27e29a4931fca790e88db809221326e9a8e
Lucker <Samir-ramazanov-1995@mail.ru>
2023-10-11 20:36:46 +0300

    Update content object

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/ContentObject.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/ContentObject.kt
@@ -5,38 +5,57 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Compact
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.rcl.nextshiki.MR.strings.description_in_object
 import com.rcl.nextshiki.MR.strings.score_in_object
 import com.rcl.nextshiki.MR.strings.status_in_object
 import com.rcl.nextshiki.MR.strings.text_empty
 import com.rcl.nextshiki.generateImageLoader
 import com.rcl.nextshiki.getString
+import com.rcl.nextshiki.models.searchobject.CharacterModel
 import com.rcl.nextshiki.models.searchobject.ObjById
-import com.rcl.nextshiki.widthSizeClass
+import com.rcl.nextshiki.screens.search.searchelement.SearchElementScreen
 import com.seiko.imageloader.LocalImageLoader
 import com.seiko.imageloader.rememberImagePainter
 
 object ContentObject {
     @Composable
-    fun ContentObject(value: ObjById) {
-        widthSizeClass
-        when (widthSizeClass.value) {
+    fun CharacterContentObject(value: CharacterModel) {
+
+    }
+    @Composable
+    fun CharMobileUI(value: CharacterModel){
+
+    }
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    @Composable
+    fun BasicContentObject(value: ObjById) {
+        val widthSizeClass = calculateWindowSizeClass().widthSizeClass
+        when (widthSizeClass) {
             Compact ->
                 mobileUI(value)
 
@@ -55,6 +74,7 @@ object ContentObject {
                 textBlock(value)
                 scoreBlock(value)
                 statusObject(value)
+                getDescription(value.description)
             }
         }
     }
@@ -70,9 +90,7 @@ object ContentObject {
             }
             LazyColumn(Modifier.fillMaxWidth().weight(1f)) {
                 item {
-                    Card(modifier = Modifier.fillMaxSize()) {
-
-                    }
+                    getDescription(value.description)
                 }
             }
         }
@@ -163,10 +181,11 @@ object ContentObject {
     }
 
     @Composable
-    private fun getDescription(value: ObjById) {
+    private fun getDescription(value: String?) {
+        val navigator = LocalNavigator.currentOrThrow
         Box(
             modifier = Modifier.padding(top = 16.dp).fillMaxWidth()
-        ){
+        ) {
             Column(
                 modifier = Modifier.fillMaxSize()
             ) {
@@ -174,10 +193,43 @@ object ContentObject {
                     style = MaterialTheme.typography.bodyLarge,
                     text = getString(description_in_object)
                 )
-                if (value.description!=null){
+                if (value != null) {
+                    val annotatedString = buildAnnotatedString {
+                        val listOpen = Regex("\\[character").findAll(value)
+                            .map { it.range.first }
+                            .toList()
+                        val listClose = Regex("""\[/character""".trimIndent()).findAll(value)
+                            .map { it.range.first }
+                            .toList()
+                        var tempValue = 0
+                        var lastOpen = 0
 
-                }
-                else{
+                        listOpen.forEach {
+                            append(value.substring(lastOpen, it - 1)+" ")
+                            val closeIndex = listClose[tempValue]
+                            tempValue++
+                            val charId = value.substring(it, closeIndex).split("character=")[1].split("\\[")[0]
+                            val name = value.substring(it, closeIndex).split("]")[1]
+                            withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                                pushStringAnnotation(tag = "id", annotation = charId)
+                                append(name)
+                            }
+                            lastOpen = closeIndex+"""[\character]""".length
+                        }
+                        append(value.substring(lastOpen, value.lastIndex))
+                    }
+
+                    ClickableText(
+                        text = annotatedString,
+                        onClick = { offset ->
+                            annotatedString.getStringAnnotations(offset, offset)
+                                .firstOrNull()?.let { span ->
+                                    navigator.push(SearchElementScreen(type = "characters", id = span.item))
+                                }
+                        },
+                        style = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.onBackground)
+                    )
+                } else {
                     Text(
                         text = getString(text_empty)
                     )
