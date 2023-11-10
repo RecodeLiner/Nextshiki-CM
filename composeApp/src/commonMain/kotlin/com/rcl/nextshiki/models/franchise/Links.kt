76a2b23247018a18f1e0236f929a640aa3d87487
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-11-10 18:13:55 +0300

    Add data class for Anime's franchise

---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/franchise/Links.kt
@@ -0,0 +1,15 @@
+package com.rcl.nextshiki.models.franchise
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Links (
+    @SerialName("id") val id: Int? = null,
+    @SerialName("source_id") val sourceId: Int? = null,
+    @SerialName("target_id") val targetId: Int? = null,
+    @SerialName("source") val source: Int? = null,
+    @SerialName("target") val target: Int? = null,
+    @SerialName("weight") val weight: Int? = null,
+    @SerialName("relation") val relation: String? = null
+)
---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/franchise/Nodes.kt
@@ -0,0 +1,16 @@
+package com.rcl.nextshiki.models.franchise
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Nodes (
+    @SerialName("id") val id: Int? = null,
+    @SerialName("date") val date: Int? = null,
+    @SerialName("name") val name: String? = null,
+    @SerialName("image_url") val imageUrl: String? = null,
+    @SerialName("url") val url: String? = null,
+    @SerialName("year") val year: Int? = null,
+    @SerialName("kind") val kind: String? = null,
+    @SerialName("weight") val weight: Int? = null
+)
