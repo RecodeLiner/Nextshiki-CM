2862ff296bf15b6f8d5067230e46cd73539ef2d8
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-11-12 19:08:03 +0300

    Update naming

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/search/searchelement/SearchElementScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/search/searchelement/SearchElementScreen.kt
@@ -10,11 +10,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
-import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.rcl.nextshiki.MR
-import com.rcl.nextshiki.elements.BasicContentObject
+import com.rcl.nextshiki.elements.basicContentObject
 import com.rcl.nextshiki.elements.GetDropdownMenu
 import com.rcl.nextshiki.elements.GetDropdownMenuItem
 import com.rcl.nextshiki.getString
@@ -88,7 +87,7 @@ class SearchElementScreen(private val type: String, private val id: String) : Se
         ) {
             Box(modifier = Modifier.padding(it)) {
                 if (vm.currObj.value != null) {
-                    BasicContentObject(vm.currObj.value!!)
+                    basicContentObject(vm.currObj.value!!)
                 }
             }
         }
