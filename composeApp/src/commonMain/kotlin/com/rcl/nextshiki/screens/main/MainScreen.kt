bb8412f18bded068a636280ec30bdc8590d3b3b1
Lucker <samir.ramazanov.04@bk.ru>
2023-11-15 00:42:23 +0300

    Update adaptive UI on the main screen

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/screens/main/MainScreen.kt
@@ -1,9 +1,11 @@
 package com.rcl.nextshiki.screens.main
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Compact
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,7 +17,9 @@ import com.rcl.nextshiki.elements.CalendarCard
 import com.rcl.nextshiki.getString
 
 class MainScreen : Screen {
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3WindowSizeClassApi::class,
+        ExperimentalLayoutApi::class
+    )
     @Composable
     override fun Content() {
         val vm = rememberScreenModel { MainViewModel() }
@@ -34,28 +38,28 @@ class MainScreen : Screen {
                 )
             }
         ) { paddings ->
-            Box(modifier = Modifier.padding(paddings)) {
-                Column {
-                    Card(
+            val isCompat = calculateWindowSizeClass().widthSizeClass == Compact
+            Box (modifier = Modifier.padding(paddings)) {
+                FlowColumn {
+                    BoxWithConstraints (
                         modifier = Modifier
-                            .fillMaxWidth()
                             .padding(horizontal = 16.dp)
-                            .height(220.dp),
                     ) {
                         AnimatedContent(!vm.calendarList.isEmpty()) {  empty ->
                             if (empty) {
                                 vm.nearTitle.value.let { nearTitle ->
                                     vm.previewName.value.let { name ->
-                                        CalendarCard(
-                                            name = name,
-                                            link = nearTitle?.anime?.image?.preview!!,
-                                            time = nearTitle.nextEpisodeAt!!
-                                        )
+                                        Box(modifier = Modifier.width(if(isCompat) maxWidth else maxWidth/2 )){
+                                            CalendarCard(
+                                                name = name,
+                                                link = nearTitle?.anime?.image?.preview!!,
+                                                time = nearTitle.nextEpisodeAt!!
+                                            )
+                                        }
                                     }
                                 }
                             } else {
                                 Box(
-                                    modifier = Modifier.fillMaxSize(),
                                     contentAlignment = Alignment.Center
                                 ) {
                                     CircularProgressIndicator()
