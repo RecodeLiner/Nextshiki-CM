823f371698d05691c55e9d91c0684515220da5ed
Lucker <samir.ramazanov.04@bk.ru>
2023-12-06 19:57:25 +0300

    update Routes for configuration-compatible

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/Routes.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/Routes.kt
@@ -1,12 +1,11 @@
-package com.rcl.nextshiki.screens
+package com.rcl.nextshiki.base
 
 import androidx.compose.ui.graphics.vector.ImageVector
-import cafe.adriel.voyager.core.screen.Screen
 import dev.icerock.moko.resources.StringResource
 
 data class Routes(
     val name: StringResource,
-    val screen: Screen,
+    val configuration: RootComponent.Configuration,
     val outlinedIcon: ImageVector,
     val filledIcon: ImageVector,
 )
