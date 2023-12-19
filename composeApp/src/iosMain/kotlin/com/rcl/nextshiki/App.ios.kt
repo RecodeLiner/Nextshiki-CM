27690aaa1b47dab184a4f8ca1454c68b91810813
RecodeLiner <testsonlyfor85@gmail.com>
2023-12-19 17:56:26 +0300

    Migrate on iosmain(for future)

--- composeApp/src/iosMain/kotlin/com/rcl/nextshiki/App.ios.kt
+++ composeApp/src/iosMain/kotlin/com/rcl/nextshiki/App.ios.kt
@@ -1,21 +1,13 @@
 package com.rcl.nextshiki
 
-import Nextshiki.composeApp.BuildConfig
 import Nextshiki.composeApp.BuildConfig.CLIENT_ID
 import Nextshiki.composeApp.BuildConfig.CLIENT_SECRET_DESK
 import Nextshiki.composeApp.BuildConfig.REDIRECT_URI
-import androidx.compose.runtime.Composable
 import com.rcl.nextshiki.di.ktor.KtorRepository
 import com.rcl.nextshiki.models.currentuser.TokenModel
 import com.seiko.imageloader.ImageLoader
 import com.seiko.imageloader.component.setupDefaultComponents
 import com.seiko.imageloader.defaultImageResultMemoryCache
-import dev.icerock.moko.resources.StringResource
-import dev.icerock.moko.resources.desc.Resource
-import dev.icerock.moko.resources.desc.StringDesc
-import dev.icerock.moko.resources.format
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import okio.Path.Companion.toPath
 import platform.Foundation.NSURL
 import platform.UIKit.UIApplication
@@ -59,4 +51,8 @@ internal actual suspend fun getToken(isFirst: Boolean, code: String): TokenModel
         clientSecret = CLIENT_SECRET_DESK,
         redirectUri = REDIRECT_URI
         )
+}
+
+internal actual fun copyToClipboard(text: String) {
+
 }
