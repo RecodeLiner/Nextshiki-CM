ae54bdfcf6e6d1bec7bf34472295eb8eb35cfbea
RecodeLiner <testsonlyfor85@gmail.com>
2023-12-19 17:53:25 +0300

    Set RootComponent for decompose

---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/RootComponent.kt
@@ -0,0 +1,68 @@
+package com.rcl.nextshiki.base
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.bringToFront
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.rcl.nextshiki.base.main.MainComponent
+import com.rcl.nextshiki.base.profile.ProfileComponent
+import com.rcl.nextshiki.base.search.SearchComponent
+import kotlinx.coroutines.MainScope
+import kotlinx.serialization.Serializable
+
+class RootComponent(context: ComponentContext) : ComponentContext by context {
+    private val navigator = StackNavigation<TopLevelConfiguration>()
+
+    val childStack = childStack(
+        source = navigator,
+        serializer = TopLevelConfiguration.serializer(),
+        initialConfiguration = TopLevelConfiguration.MainScreen,
+        handleBackButton = true,
+        childFactory = ::createChild
+    )
+
+    fun navigateTo(config: TopLevelConfiguration) {
+        navigator.bringToFront(config)
+    }
+
+    fun onBack() {
+        navigator.pop()
+    }
+
+    private fun createChild(
+        config: TopLevelConfiguration,
+        context: ComponentContext,
+    ): TopLevelChild {
+        return when (config) {
+            TopLevelConfiguration.MainScreen -> TopLevelChild.MainScreen(
+                MainComponent(context = context, mainContext = MainScope().coroutineContext)
+            )
+            TopLevelConfiguration.SearchScreen -> TopLevelChild.SearchScreen(
+                SearchComponent(context = context)
+            )
+            TopLevelConfiguration.ProfileScreen -> TopLevelChild.ProfileScreen(
+                ProfileComponent(context = context)
+            )
+        }
+    }
+
+    sealed class TopLevelChild {
+        data class MainScreen(val component: MainComponent) : TopLevelChild()
+        data class SearchScreen(val component: SearchComponent) : TopLevelChild()
+        data class ProfileScreen(val component: ProfileComponent) : TopLevelChild()
+    }
+
+    @Serializable
+    sealed interface TopLevelConfiguration {
+
+        @Serializable
+        data object MainScreen : TopLevelConfiguration
+
+        @Serializable
+        data object SearchScreen : TopLevelConfiguration
+
+        @Serializable
+        data object ProfileScreen : TopLevelConfiguration
+    }
+}
