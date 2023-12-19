9341b1ba7b35c1a277d8b2663d0a92267d65b354
RecodeLiner <testsonlyfor85@gmail.com>
2023-12-19 18:00:17 +0300

    Temp remove login to refactor

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/LoginObject.kt
+++
@@ -1,7 +0,0 @@
-package com.rcl.nextshiki.elements
-
-import androidx.compose.runtime.Composable
-import com.rcl.nextshiki.screens.profile.ProfileViewModel
-
-@Composable
-internal expect fun LoginObject(vm: ProfileViewModel)
