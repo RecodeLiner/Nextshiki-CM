6547261e52ba2325d94e1f7804605081a4481622
RecodeLiner <testsonlyfor85@gmail.com>
2023-12-19 18:00:36 +0300

    Interface for settings on decompose

---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/ISettings.kt
@@ -0,0 +1,9 @@
+package com.rcl.nextshiki.base.profile.settings
+
+import com.rcl.nextshiki.base.profile.settings.subcomponents.ButtonComponent
+import com.rcl.nextshiki.base.profile.settings.subcomponents.FullLangRowComponent
+
+interface ISettings {
+    val tokenButton : ButtonComponent
+    val langRowComponent : FullLangRowComponent
+}
