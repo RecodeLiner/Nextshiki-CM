1382a4940937b520be5efd19ea66fc2274f9be0f
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-12-19 17:54:43 +0300

    Migrate to decompose with lifecycle on desktop main

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,13 +1,29 @@
+import Nextshiki.composeApp.BuildConfig
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import com.rcl.nextshiki.app
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.rcl.nextshiki.App
+import com.rcl.nextshiki.base.RootComponent
 
+@OptIn(ExperimentalDecomposeApi::class)
 fun main() = application {
+    val lifecycle = LifecycleRegistry()
+    val windowState = rememberWindowState(width = 800.dp, height = 600.dp)
+    LifecycleController(lifecycle, windowState)
     Window(
-        title = "Nextshiki",
-        state = rememberWindowState(width = 800.dp, height = 600.dp),
+        title = BuildConfig.USER_AGENT,
+        state = windowState,
         onCloseRequest = ::exitApplication,
-    ) { app() }
+    ) {
+        val root = remember {
+            RootComponent(DefaultComponentContext(lifecycle))
+        }
+        App(root)
+    }
 }
