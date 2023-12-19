a3f61943f5f7feab22fbb9d34e608d8213c61bcd
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2023-12-19 18:01:30 +0300

    Language row in settings(not fully completed)

---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/subcomponents/IFullLangRow.kt
@@ -0,0 +1,7 @@
+package com.rcl.nextshiki.base.profile.settings.subcomponents
+
+interface IFullLangRow {
+    val ruButton: LangRowComponent
+    val engButton: LangRowComponent
+    val resetButton: LangRowComponent
+}
---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/subcomponents/ILangRow.kt
@@ -0,0 +1,6 @@
+package com.rcl.nextshiki.base.profile.settings.subcomponents
+
+interface ILangRow {
+    val lang: String
+    val lang_code: String?
+}
