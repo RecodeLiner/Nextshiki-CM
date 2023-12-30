e6e51ed300da684eb798f4f283f4212142211bef
RecodeLiner <RecodeLIner@gmail.com>
2023-12-30 14:37:23 +0300

    Update search interface

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/IMainSearch.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/IMainSearch.kt
@@ -1,8 +1,14 @@
 package com.rcl.nextshiki.base.search.mainsearchscreen
 
 import com.arkivanov.decompose.value.Value
+import com.rcl.nextshiki.models.searchobject.SearchListItem
 
 interface IMainSearch {
+    val currentType: Value<SearchType>
+    val list: MutableList<SearchListItem>
     var text: Value<String>
     fun onTextChanged(value: String)
+    fun updateType(type: SearchType)
+    fun clearList()
+    fun searchObject(text: String)
 }
