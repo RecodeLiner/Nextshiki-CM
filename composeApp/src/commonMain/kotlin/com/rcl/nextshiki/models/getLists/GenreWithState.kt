dcdaf87f9fe59a92e7271d509cb5cf74718edfb4
RecodeLiner <RecodeLIner@gmail.com>
2024-01-02 03:14:51 +0300

    Remove default value in GenreWithState

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/getLists/GenreWithState.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/getLists/GenreWithState.kt
@@ -4,5 +4,5 @@ import androidx.compose.ui.state.ToggleableState
 
 data class GenreWithState(
     val obj: ListGenresItem,
-    val state: ToggleableState = ToggleableState.Off
+    val state: ToggleableState
 )
