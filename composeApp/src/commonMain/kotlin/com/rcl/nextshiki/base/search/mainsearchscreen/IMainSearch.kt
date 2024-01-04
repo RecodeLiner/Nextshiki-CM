5a2b8f46a697b2fbdefd9c465363596428e853c7
RecodeLiner <RecodeLIner@gmail.com>
2024-01-04 23:47:58 +0300

    Replace old list with search card model list + add genrelist for filters

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/IMainSearch.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/IMainSearch.kt
@@ -1,11 +1,14 @@
 package com.rcl.nextshiki.base.search.mainsearchscreen
 
 import com.arkivanov.decompose.value.Value
-import com.rcl.nextshiki.models.searchobject.SearchListItem
+import com.rcl.nextshiki.models.getLists.GenreWithState
+import com.rcl.nextshiki.models.searchobject.SearchCardModel
 
 interface IMainSearch {
+    val typeList: List<SearchType>
     val currentType: Value<SearchType>
-    val list: MutableList<SearchListItem>
+    val searchedList: MutableList<SearchCardModel>
+    val genresList: MutableList<GenreWithState>
     var text: Value<String>
     fun onTextChanged(value: String)
     fun updateType(type: SearchType)
