b8c6d5a2fa61cd65158eeac80d59763a37b19f8f
RecodeLiner <RecodeLIner@gmail.com>
2024-01-06 07:57:47 +0300

    Replace old arraylist in models with base List

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/searchobject/CharacterModel.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/searchobject/CharacterModel.kt
@@ -20,7 +20,7 @@ data class CharacterModel (
     @SerialName("thread_id") val threadId: Int? = null,
     @SerialName("topic_id") val topicId: Int? = null,
     @SerialName("updated_at") val updatedAt: String? = null,
-    @SerialName("seyu") val seyu: ArrayList<Seyu> = arrayListOf(),
-    @SerialName("animes") val animes: ArrayList<CharAnimes> = arrayListOf(),
-    @SerialName("mangas") val mangas: ArrayList<CharMangas> = arrayListOf()
+    @SerialName("seyu") val seyu: List<Seyu> = arrayListOf(),
+    @SerialName("animes") val animes: List<CharAnimes> = arrayListOf(),
+    @SerialName("mangas") val mangas: List<CharMangas> = arrayListOf()
 )
