1dd6e4f04a28c14330ef5cd64b75c24480919303
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-01-12 17:08:43 +0300

    Update settings for add return back function +little refactor

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/SettingsComponent.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/SettingsComponent.kt
@@ -1,10 +1,18 @@
 package com.rcl.nextshiki.base.profile.settings
 
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.bringToFront
+import com.rcl.nextshiki.base.profile.ProfileComponent
 import com.rcl.nextshiki.base.profile.settings.subcomponents.ButtonComponent
 import com.rcl.nextshiki.base.profile.settings.subcomponents.FullLangRowComponent
 import com.rcl.nextshiki.strings.MainResStrings.settings_copy_token
 
-class SettingsComponent : ISettings {
+class SettingsComponent(
+    val navigator: StackNavigation<ProfileComponent.ProfileConfiguration>
+) : ISettings {
     override val tokenButton = ButtonComponent(settings_copy_token)
     override val langRowComponent = FullLangRowComponent()
+    fun returnToProfile(){
+        navigator.bringToFront(ProfileComponent.ProfileConfiguration.MainProfileScreen)
+    }
 }
