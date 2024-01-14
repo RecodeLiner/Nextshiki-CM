de0c60283874585903b62177c944059a5637342b
RecodeLiner <RecodeLIner@gmail.com>
2024-01-14 17:29:50 +0300

    Update preview

--- composeApp/src/desktopMain/kotlin/desktopUIpreview.kt
+++ composeApp/src/desktopMain/kotlin/desktopUIpreview.kt
@@ -1,13 +1,14 @@
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.rcl.nextshiki.elements.CalendarCard
 import com.rcl.nextshiki.theme.Theme.AppTheme
 
 @Composable
 @Preview
 fun CalendarCardPreview(){
-    AppTheme{
+    AppTheme(seedColor = Color.Blue){
         CalendarCard("temp", "/system/animes/original/56579.jpg?1694161076", "00")
     }
 }
