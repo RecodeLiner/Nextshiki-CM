a7323dc33bd393b44b3ce5ddbcef106b69b90a16
RecodeLiner <RecodeLIner@gmail.com>
2024-01-23 20:42:45 +0300

    Update build gradle file

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -160,8 +160,8 @@ android {
         res.srcDirs("src/androidMain/resources")
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildTypes {
         release {
