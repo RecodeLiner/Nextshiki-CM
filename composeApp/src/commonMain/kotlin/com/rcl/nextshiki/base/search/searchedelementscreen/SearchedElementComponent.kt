84eb39719e0d352407107c7f76063d3759fe0ce3
RecodeLiner <RecodeLIner@gmail.com>
2024-01-25 20:11:36 +0300

    Add unified methods in a searched model component

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/searchedelementscreen/SearchedElementComponent.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/searchedelementscreen/SearchedElementComponent.kt
@@ -11,11 +11,8 @@ import com.rcl.nextshiki.base.coroutineScope
 import com.rcl.nextshiki.base.search.SearchComponent
 import com.rcl.nextshiki.base.search.mainsearchscreen.SearchType
 import com.rcl.nextshiki.di.ktor.KtorRepository
-import com.rcl.nextshiki.models.searchobject.anime.AnimeObject
-import com.rcl.nextshiki.models.searchobject.manga.MangaObject
-import com.rcl.nextshiki.models.searchobject.people.PeopleObject
-import com.rcl.nextshiki.models.searchobject.ranobe.RanobeObject
-import com.rcl.nextshiki.models.searchobject.users.UserObject
+import com.rcl.nextshiki.models.searchobject.CommonSearchInterface
+import com.rcl.nextshiki.models.searchobject.SimpleSearchModel
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
@@ -26,16 +23,8 @@ class SearchedElementComponent(
     context: ComponentContext,
     val navigator: StackNavigation<SearchComponent.SearchConfiguration>,
 ) : ComponentContext by context, ISearchedElement {
-    private var _searchedAnimeElement = MutableValue(AnimeObject())
-    private var _searchedMangaElement = MutableValue(MangaObject())
-    private var _searchedRanobeElement = MutableValue(RanobeObject())
-    private var _searchedPeopleElement = MutableValue(PeopleObject())
-    private var _searchedUserElement = MutableValue(UserObject())
-    override val searchedAnimeElement = _searchedAnimeElement
-    override val searchedMangaElement = _searchedMangaElement
-    override val searchedRanobeElement = _searchedRanobeElement
-    override val searchedPeopleElement = _searchedPeopleElement
-    override val searchedUserElement = _searchedUserElement
+    private var _searchedElement = MutableValue<CommonSearchInterface>(SimpleSearchModel())
+    override val searchedElement = _searchedElement
 
     override fun popBack() {
         navigator.pop()
@@ -48,41 +37,25 @@ class SearchedElementComponent(
             coroutine.launch{
                 when(type){
                     SearchType.Anime -> {
-                        _searchedAnimeElement.value = koin.get<KtorRepository>().getAnimeById(id)
+                        _searchedElement.value = koin.get<KtorRepository>().getAnimeById(id)
                     }
                     SearchType.Manga -> {
-                        _searchedMangaElement.value = koin.get<KtorRepository>().getMangaById(id)
+                        _searchedElement.value = koin.get<KtorRepository>().getMangaById(id)
                     }
                     SearchType.Ranobe -> {
-                        _searchedRanobeElement.value = koin.get<KtorRepository>().getRanobeById(id)
+                        _searchedElement.value = koin.get<KtorRepository>().getRanobeById(id)
                     }
                     SearchType.People -> {
-                        _searchedPeopleElement.value = koin.get<KtorRepository>().getPeopleById(id)
+                        _searchedElement.value = koin.get<KtorRepository>().getPeopleById(id)
                     }
                     SearchType.Users -> {
-                        _searchedUserElement.value = koin.get<KtorRepository>().getUserById(id)
+                        _searchedElement.value = koin.get<KtorRepository>().getUserById(id)
                     }
                 }
             }
         }
         lifecycle.doOnDestroy {
-            when(type){
-                SearchType.Anime -> {
-                    _searchedAnimeElement.value = AnimeObject()
-                }
-                SearchType.Manga -> {
-                    _searchedMangaElement.value = MangaObject()
-                }
-                SearchType.Ranobe -> {
-                    _searchedRanobeElement.value = RanobeObject()
-                }
-                SearchType.People -> {
-                    _searchedPeopleElement.value = PeopleObject()
-                }
-                SearchType.Users -> {
-                    _searchedUserElement.value = UserObject()
-                }
-            }
+            _searchedElement.value = SimpleSearchModel()
         }
     }
 }
