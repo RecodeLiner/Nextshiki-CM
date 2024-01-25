ce140d1b36b0bb4bfb285863e08d5a1b08d89456
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-01-25 20:58:47 +0300

    Update class to dataclass

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/subelements/CardElement.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/subelements/CardElement.kt
@@ -1,6 +1,7 @@
 package com.rcl.nextshiki.base.main.subelements
 
-class CardElement(
-    override var name: String = "", override var imageLink: String = "",
+data class CardElement(
+    override var name: String = "",
+    override var imageLink: String = "",
     override var nextEpisodeAt: String = ""
 ) : ICard
