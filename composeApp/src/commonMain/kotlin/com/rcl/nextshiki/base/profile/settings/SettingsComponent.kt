28a0ba554720368271175d1e8874b49194539296
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-02-03 02:50:34 +0300

    Add methods to change language

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/SettingsComponent.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/SettingsComponent.kt
@@ -2,10 +2,12 @@ package com.rcl.nextshiki.base.profile.settings
 
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.router.stack.bringToFront
+import com.rcl.moko.MR.strings.settings_copy_token
 import com.rcl.nextshiki.base.profile.ProfileComponent
 import com.rcl.nextshiki.base.profile.settings.subcomponents.ButtonComponent
 import com.rcl.nextshiki.base.profile.settings.subcomponents.FullLangRowComponent
-import com.rcl.nextshiki.strings.MainResStrings.settings_copy_token
+import dev.icerock.moko.resources.desc.StringDesc
+import dev.icerock.moko.resources.desc.StringDesc.LocaleType.System
 
 class SettingsComponent(
     val navigator: StackNavigation<ProfileComponent.ProfileConfiguration>
@@ -14,5 +16,18 @@ class SettingsComponent(
     override val langRowComponent = FullLangRowComponent()
     fun returnToProfile(){
         navigator.bringToFront(ProfileComponent.ProfileConfiguration.MainProfileScreen)
+    }
+
+    override fun setupLanguage(code: String?) {
+        if (code!=null) {
+            StringDesc.localeType = StringDesc.LocaleType.Custom(code)
+        }
+        else {
+            StringDesc.localeType = System
+        }
+    }
+
+    override fun clearLanguage() {
+        StringDesc.localeType = System
     }
 }
