331dbfe81c7d20738c6c451863472e921d97777b
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-02-06 03:51:05 +0300

    Replace compose image loader with coil

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/CalendarCardObject.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/CalendarCardObject.kt
@@ -1,24 +1,23 @@
 package com.rcl.nextshiki.elements
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ContentScale.Companion.Crop
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
+import coil3.request.ImageRequest
+import coil3.size.Size
 import com.rcl.moko.MR.strings.future_calendar
 import com.rcl.moko.MR.strings.past_calendar
-import com.seiko.imageloader.LocalImageLoader
-import com.seiko.imageloader.rememberImagePainter
 import dev.icerock.moko.resources.compose.stringResource
 import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
@@ -34,17 +33,15 @@ fun CalendarCard(name: String, link: String, time: String) = Card(
     val ins = LocalDateTime.parse(time.split(".")[0]).toInstant(TimeZone.currentSystemDefault())
     val isPast = ins.minus(Clock.System.now()).isNegative()
     Box(modifier = Modifier.fillMaxSize()){
-        CompositionLocalProvider(
-            LocalImageLoader provides remember { generateImageLoader() },
-        ) {
-            val painter = rememberImagePainter(link)
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = painter,
-                contentDescription = "Calendar preview image",
-                contentScale = ContentScale.Crop
-            )
-        }
+        AsyncImage(
+            model = ImageRequest.Builder(LocalPlatformContext.current)
+                .data(link)
+                .size(Size.ORIGINAL)
+                .build(),
+            contentDescription = "Calendar preview image",
+            contentScale = Crop,
+            modifier = Modifier.fillMaxWidth()
+        )
         Spacer(
             modifier = Modifier
                 .align(Alignment.BottomStart)
