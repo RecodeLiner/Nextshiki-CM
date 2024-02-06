05ea0a770e1fe90bbc855d5b8240b0d0eb68b233
RecodeLiner <RecodeLIner@gmail.com>
2024-02-06 09:32:05 +0300

    Completely remove old methods

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.kt
@@ -1,10 +1,5 @@
 package com.rcl.nextshiki.elements
 
-import com.rcl.nextshiki.models.currentuser.TokenModel
-import com.seiko.imageloader.ImageLoader
 
 internal expect fun openUrl(url: String?)
-internal expect fun generateImageLoader(): ImageLoader
-
-internal expect suspend fun getToken(isFirst: Boolean, code: String): TokenModel
 internal expect fun copyToClipboard(text: String)
