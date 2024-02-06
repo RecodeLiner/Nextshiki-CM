c9fbc6192fa177acd70e76fef515c5fa61b022bc
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-02-06 09:32:05 +0300

    Completely remove old methods

--- composeApp/src/iosMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.ios.kt
+++ composeApp/src/iosMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.ios.kt
@@ -1,7 +1,6 @@
 package com.rcl.nextshiki.elements
 
 import Nextshiki.composeApp.BuildConfig
-import com.rcl.nextshiki.di.Koin.getSafeKoin
 import com.rcl.nextshiki.di.ktor.KtorRepository
 import com.rcl.nextshiki.models.currentuser.TokenModel
 import com.seiko.imageloader.ImageLoader
@@ -14,40 +13,12 @@ internal actual fun openUrl(url: String?) {
     UIApplication.sharedApplication.openURL(nsUrl)
 }
 
-internal actual fun generateImageLoader(): ImageLoader {
-    return ImageLoader {
-        components {
-            setupDefaultComponents()
-        }
-        interceptor {
-            defaultImageResultMemoryCache()
-            memoryCacheConfig {
-                maxSizeBytes(32 * 1024 * 1024)
-            }
-            diskCacheConfig {
-                directory(getCacheDir().toPath().resolve("image_cache"))
-                maxSizeBytes(512L * 1024 * 1024)
-            }
-        }
-    }
-}
-
 private fun getCacheDir(): String {
     return NSSearchPathForDirectoriesInDomains(
         NSCachesDirectory,
         NSUserDomainMask,
         true,
     ).first() as String
-}
-
-internal actual suspend fun getToken(isFirst: Boolean, code: String): TokenModel {
-    return getSafeKoin().get<KtorRepository>().getToken(
-        isFirst = isFirst,
-        code = code,
-        clientID = BuildConfig.CLIENT_ID,
-        clientSecret = BuildConfig.CLIENT_SECRET_DESK,
-        redirectUri = BuildConfig.REDIRECT_URI
-    )
 }
 
 internal actual fun copyToClipboard(text: String) {
