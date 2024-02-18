ceb0fe97d203f81df729494f5d120cf37f98e8cc
RecodeLiner <RecodeLIner@gmail.com>
2024-02-18 07:39:20 +0300

    Add replace link from android's assist content

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/searchedelementscreen/SearchedElementComponent.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/searchedelementscreen/SearchedElementComponent.kt
@@ -1,5 +1,6 @@
 package com.rcl.nextshiki.base.search.searchedelementscreen
 
+import Nextshiki.composeApp.BuildConfig.DOMAIN
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.router.stack.pop
@@ -9,6 +10,7 @@ import com.arkivanov.essenty.lifecycle.doOnDestroy
 import com.rcl.nextshiki.base.search.SearchComponent
 import com.rcl.nextshiki.base.search.mainsearchscreen.SearchType
 import com.rcl.nextshiki.di.ktor.KtorRepository
+import com.rcl.nextshiki.elements.setOutContent
 import com.rcl.nextshiki.models.searchobject.CommonSearchInterface
 import com.rcl.nextshiki.models.searchobject.SimpleSearchModel
 import kotlinx.coroutines.CoroutineScope
@@ -35,20 +37,24 @@ class SearchedElementComponent(
 
     init {
         lifecycle.doOnCreate {
-            coroutine.launch{
-                when(type){
+            coroutine.launch {
+                when (type) {
                     SearchType.Anime -> {
                         _searchedElement.value = ktorRepository.getAnimeById(id)
                     }
+
                     SearchType.Manga -> {
                         _searchedElement.value = ktorRepository.getMangaById(id)
                     }
+
                     SearchType.Ranobe -> {
                         _searchedElement.value = ktorRepository.getRanobeById(id)
                     }
+
                     SearchType.People -> {
                         _searchedElement.value = ktorRepository.getPeopleById(id)
                     }
+
                     SearchType.Users -> {
                         _searchedElement.value = ktorRepository.getUserById(id)
                     }
@@ -57,6 +63,17 @@ class SearchedElementComponent(
         }
         lifecycle.doOnDestroy {
             _searchedElement.value = SimpleSearchModel()
+        }
+        lifecycle.setOutContent("$DOMAIN/${type.getTypePath()}/$id")
+    }
+
+    private fun SearchType.getTypePath(): String {
+        return when (this) {
+            SearchType.Anime -> "animes"
+            SearchType.Manga -> "mangas"
+            SearchType.Ranobe -> "ranobe"
+            SearchType.People -> "people"
+            SearchType.Users -> "users"
         }
     }
 }
