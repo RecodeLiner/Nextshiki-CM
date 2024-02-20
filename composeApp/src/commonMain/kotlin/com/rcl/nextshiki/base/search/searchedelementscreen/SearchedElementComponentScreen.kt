cb1aa0e57aded9daa45e8cdfca93e33e7ace5b91
RecodeLiner <RecodeLIner@gmail.com>
2024-02-20 23:14:54 +0300

    optimize imports

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/searchedelementscreen/SearchedElementComponentScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/searchedelementscreen/SearchedElementComponentScreen.kt
@@ -4,7 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
