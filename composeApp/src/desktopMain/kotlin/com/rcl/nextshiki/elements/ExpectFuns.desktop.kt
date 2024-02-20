b961110b8411dea8bb52fecb15ef815939b874b0
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-02-20 19:36:58 +0300

    remove openurl method(requires context for android) + remove cache dir on ios

--- composeApp/src/desktopMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.desktop.kt
@@ -6,11 +6,6 @@ import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.StringSelection
 import java.net.URI
 
-internal actual fun openUrl(url: String?) {
-    val uri = url?.let { URI.create(it) } ?: return
-    Desktop.getDesktop().browse(uri)
-}
-
 
 internal actual fun copyToClipboard(text: String) {
     val selection = StringSelection(text)
