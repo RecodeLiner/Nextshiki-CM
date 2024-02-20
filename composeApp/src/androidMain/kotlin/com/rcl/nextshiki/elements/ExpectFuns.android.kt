a6d4855588025063cc124b65762f95f86051820b
RecodeLiner <RecodeLIner@gmail.com>
2024-02-20 23:14:14 +0300

    Remove imports

--- composeApp/src/androidMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.android.kt
+++ composeApp/src/androidMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.android.kt
@@ -1,12 +1,7 @@
 package com.rcl.nextshiki.elements
 
 import android.content.ClipData
-import android.content.Intent
-import android.net.Uri
-import androidx.core.content.ContextCompat.startActivity
-import com.rcl.nextshiki.AndroidApp
 import com.rcl.nextshiki.AndroidApp.Companion.clipboardManager
-import org.koin.core.context.GlobalContext
 
 
 internal actual fun copyToClipboard(text: String) {
