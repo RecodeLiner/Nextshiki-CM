08d9f4a419fca9f7b93bf5ee2692ca653c6c4f95
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-02-25 16:16:09 +0300

    Add add co-args for ktor

---
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/topics/ForumType.kt
@@ -0,0 +1,18 @@
+package com.rcl.nextshiki.models.topics
+
+enum class ForumType {
+    all,
+    animanga,
+    site,
+    games,
+    vn,
+    contests,
+    offtopic,
+    clubs,
+    my_clubs,
+    critiques,
+    news,
+    collections,
+    articles,
+    cosplay
+}
