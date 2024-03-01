745cd9be4803c22cf0912a4ee4adc29904219f59
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-03-01 22:56:57 +0300

    Replace old main view with modern test(only for test!!)

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/IMain.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/IMain.kt
@@ -1,10 +1,9 @@
 package com.rcl.nextshiki.base.main.mainpage
 
-import com.arkivanov.decompose.value.Value
 import com.rcl.nextshiki.base.main.mainpage.subelements.CardElement
 import com.rcl.nextshiki.models.topics.HotTopics
 
 interface IMain {
-    val cardElement: Value<CardElement>
+    val cardList: MutableList<CardElement>
     val topicsList: MutableList<HotTopics>
 }
--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/MainNewsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/MainNewsScreen.kt
@@ -1,66 +1,71 @@
 package com.rcl.nextshiki.base.main.mainpage
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion.Compact
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import com.rcl.nextshiki.elements.CalendarCard
+import com.rcl.nextshiki.elements.TopicCard
+import io.github.aakira.napier.Napier
+import io.kamel.core.isSuccess
+import io.kamel.image.asyncPainterResource
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun MainNewsComponentScreen(component: MainNewsComponent) {
-    val size = calculateWindowSizeClass().widthSizeClass
+    val calendarList = component.cardList
+    val newsList = component.topicsList
+    val coroutineScope = rememberCoroutineScope()
 
-    when (size) {
-        Compact -> {
-            mobileView(component)
-        }
-
-        else -> {
-            desktopView(component)
-        }
-    }
-}
-
-@Composable
-private fun mobileView(component: MainNewsComponent) {
-    val card by component.cardElement.subscribeAsState()
-    LazyColumn {
-        item {
-            Card(modifier = Modifier.fillMaxWidth().height(220.dp)) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        val rowState = rememberLazyListState()
+        LazyRow(
+            state = rowState, modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp)
+                .draggable(
+                    orientation = Orientation.Horizontal,
+                    state = rememberDraggableState { delta ->
+                        coroutineScope.launch {
+                            rowState.scrollBy(-delta)
+                        }
+                    },
+                ),
+        ) {
+            items(calendarList) { card ->
                 if (card.name.isNotEmpty() && card.imageLink.isNotEmpty() && card.nextEpisodeAt.isNotEmpty()) {
                     CalendarCard(name = card.name, link = card.imageLink, time = card.nextEpisodeAt)
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun desktopView(component: MainNewsComponent) {
-    val card by component.cardElement.subscribeAsState()
-    Row {
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            item {
-                Card(modifier = Modifier.fillMaxWidth().height(220.dp)) {
-                    if (card.name.isNotEmpty() && card.imageLink.isNotEmpty() && card.nextEpisodeAt.isNotEmpty()) {
-                        CalendarCard(name = card.name, link = card.imageLink, time = card.nextEpisodeAt)
+        Spacer(modifier = Modifier.height(10.dp))
+        LazyVerticalGrid(columns = GridCells.Adaptive(150.dp)) {
+            items(newsList) { topic ->
+                val painter =
+                    topic.htmlFooter?.let { it -> component.extractLink(it)?.let { asyncPainterResource(data = it) } }
+                if (painter?.isSuccess == true) {
+                    if (topic.topicTitle != null) {
+                        if (topic.user != null) {
+                            TopicCard(
+                                onClick = { Napier.i("success: ${topic.id}") },
+                                image = painter,
+                                title = topic.topicTitle,
+                                user = topic.user
+                            )
+                        }
                     }
                 }
-            }
-        }
-        LazyColumn(modifier = Modifier.weight(1f)) {
-            item {
-
             }
         }
     }
