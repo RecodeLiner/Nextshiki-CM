62a9e31a820a4aa44bfa426008b4f9a4bf7f4100
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-03-09 17:37:00 +0300

    Update Images

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/MainNewsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/MainNewsScreen.kt
@@ -11,15 +11,20 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImagePainter.State.*
+import coil3.compose.LocalPlatformContext
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.size.Size
 import com.rcl.nextshiki.elements.CalendarCard
 import com.rcl.nextshiki.elements.TopicCard
 import io.github.aakira.napier.Napier
-import io.kamel.core.isSuccess
-import io.kamel.image.asyncPainterResource
 import kotlinx.coroutines.launch
 
 @Composable
@@ -31,7 +36,8 @@ fun MainNewsComponentScreen(component: MainNewsComponent) {
     Column(modifier = Modifier.fillMaxSize()) {
         val rowState = rememberLazyListState()
         LazyRow(
-            state = rowState, modifier = Modifier
+            state = rowState,
+            modifier = Modifier
                 .fillMaxWidth()
                 .height(220.dp)
                 .draggable(
@@ -44,26 +50,67 @@ fun MainNewsComponentScreen(component: MainNewsComponent) {
                 ),
         ) {
             items(calendarList) { card ->
-                if (card.name.isNotEmpty() && card.imageLink.isNotEmpty() && card.nextEpisodeAt.isNotEmpty()) {
-                    CalendarCard(name = card.name, link = card.imageLink, time = card.nextEpisodeAt)
+                Card (modifier = Modifier.fillMaxHeight().width(500.dp)) {
+                    if (card.name.isNotEmpty() && card.imageLink.isNotEmpty() && card.nextEpisodeAt.isNotEmpty()) {
+                        val painter = rememberAsyncImagePainter(
+                            ImageRequest
+                                .Builder(LocalPlatformContext.current)
+                                .data(card.imageLink)
+                                .size(Size.ORIGINAL)
+                                .build()
+                        )
+                        when (painter.state) {
+                            is Success -> {
+                                CalendarCard(name = card.name, painter = painter, time = card.nextEpisodeAt)
+                            }
+                            is Empty -> {
+
+                            }
+
+                            is Error -> {
+
+                            }
+
+                            is Loading -> {
+                                CircularProgressIndicator()
+                            }
+                        }
+                    }
                 }
             }
         }
         Spacer(modifier = Modifier.height(10.dp))
         LazyVerticalGrid(columns = GridCells.Adaptive(150.dp)) {
             items(newsList) { topic ->
-                val painter =
-                    topic.htmlFooter?.let { it -> component.extractLink(it)?.let { asyncPainterResource(data = it) } }
-                if (painter?.isSuccess == true) {
-                    if (topic.topicTitle != null) {
-                        if (topic.user != null) {
-                            TopicCard(
-                                onClick = { Napier.i("success: ${topic.id}") },
-                                image = painter,
-                                title = topic.topicTitle,
-                                user = topic.user
-                            )
+                val painter = rememberAsyncImagePainter(
+                    ImageRequest
+                        .Builder(LocalPlatformContext.current)
+                        .data(topic.htmlFooter?.let { component.extractLink(it) })
+                        .size(Size.ORIGINAL)
+                        .build()
+                )
+
+                when (painter.state) {
+                    is Success -> {
+                        if (topic.topicTitle != null) {
+                            if (topic.user != null) {
+                                TopicCard(
+                                    onClick = { Napier.i("success: ${topic.id}") },
+                                    painter = painter,
+                                    title = topic.topicTitle,
+                                    user = topic.user
+                                )
+                            }
                         }
+                    }
+                    is Empty -> {
+
+                    }
+                    is Error -> {
+
+                    }
+                    is Loading -> {
+                        CircularProgressIndicator()
                     }
                 }
             }
