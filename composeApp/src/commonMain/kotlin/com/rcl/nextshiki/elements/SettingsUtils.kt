805b04932284068067e59c6c6c0873137cecdc5f
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-03-12 23:02:31 +0300

    Remove unused files

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/SettingsUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.rcl.nextshiki.elements
-
-import androidx.compose.runtime.mutableStateOf
-import com.russhwolf.settings.Settings
-
-val settings: Settings = Settings()
-
-val currLink = mutableStateOf<String?>(null)
