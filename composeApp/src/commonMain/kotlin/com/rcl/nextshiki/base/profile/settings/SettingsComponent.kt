35dab47bed15b1f086de2235977578c0a447a2ad
RecodeLiner <RecodeLIner@gmail.com>
2024-03-14 15:17:07 +0300

    Update profile components

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/SettingsComponent.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/profile/settings/SettingsComponent.kt
@@ -1,33 +1,25 @@
 package com.rcl.nextshiki.base.profile.settings
 
-import com.arkivanov.decompose.router.stack.StackNavigation
-import com.arkivanov.decompose.router.stack.bringToFront
 import com.rcl.moko.MR.strings.settings_copy_token
-import com.rcl.nextshiki.base.profile.ProfileComponent
 import com.rcl.nextshiki.base.profile.settings.subcomponents.ButtonComponent
 import com.rcl.nextshiki.base.profile.settings.subcomponents.FullLangRowComponent
 import dev.icerock.moko.resources.desc.StringDesc
 import dev.icerock.moko.resources.desc.StringDesc.LocaleType.System
 
 class SettingsComponent(
-    val navigator: StackNavigation<ProfileComponent.ProfileConfiguration>
-) : ISettings {
-    override val tokenButton = ButtonComponent(settings_copy_token)
-    override val langRowComponent = FullLangRowComponent()
-    fun returnToProfile(){
-        navigator.bringToFront(ProfileComponent.ProfileConfiguration.MainProfileScreen)
+    val navigateToProfile: () -> Unit
+) {
+    val tokenButton = ButtonComponent(settings_copy_token)
+    val langRowComponent = FullLangRowComponent()
+    fun returnToProfile() {
+        navigateToProfile()
     }
 
-    override fun setupLanguage(code: String?) {
-        if (code!=null) {
+    fun setupLanguage(code: String?) {
+        if (code != null) {
             StringDesc.localeType = StringDesc.LocaleType.Custom(code)
-        }
-        else {
+        } else {
             StringDesc.localeType = System
         }
-    }
-
-    override fun clearLanguage() {
-        StringDesc.localeType = System
     }
 }
