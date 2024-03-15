f953ed9b89a698b520f673cc723a75fe355935ee
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-03-15 19:39:25 +0300

    Update ExpectFuns.kt

--- composeApp/src/androidMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.android.kt
+++ composeApp/src/androidMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.android.kt
@@ -1,8 +1,16 @@
 package com.rcl.nextshiki.elements
 
+import Nextshiki.composeApp.BuildConfig
+import Nextshiki.composeApp.BuildConfig.CLIENT_ID
+import Nextshiki.composeApp.BuildConfig.CLIENT_SECRET
+import Nextshiki.composeApp.BuildConfig.REDIRECT_URI
 import android.content.ClipData
 import com.rcl.nextshiki.AndroidApp.Companion.clipboardManager
+import com.rcl.nextshiki.di.ktor.KtorModel
+import com.rcl.nextshiki.di.ktor.KtorRepository
 import com.rcl.nextshiki.elements.Platforms.Mobile
+import com.russhwolf.settings.Settings
+import com.russhwolf.settings.set
 
 
 internal actual fun copyToClipboard(text: String) {
@@ -12,4 +20,23 @@ internal actual fun copyToClipboard(text: String) {
 
 internal actual fun currentPlatform(): Platforms {
     return Mobile
+}
+
+internal actual suspend fun updateToken(ktorRepository: KtorRepository, settings: Settings) {
+    val code = settings.getStringOrNull("refCode") ?: return
+
+    val token = ktorRepository.getToken(
+        code = code,
+        clientSecret = CLIENT_SECRET,
+        clientID = CLIENT_ID,
+        redirectUri = REDIRECT_URI,
+    )
+
+    settings["refCode"] = token.refreshToken
+    if (token.accessToken != null){
+        KtorModel.token.value = token.accessToken
+    }
+    if (token.scope != null) {
+        KtorModel.scope.value = token.scope
+    }
 }
--- composeApp/src/desktopMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/rcl/nextshiki/elements/ExpectFuns.desktop.kt
@@ -1,6 +1,13 @@
 package com.rcl.nextshiki.elements
 
+import Nextshiki.composeApp.BuildConfig.CLIENT_ID_DESK
+import Nextshiki.composeApp.BuildConfig.CLIENT_SECRET_DESK
+import Nextshiki.composeApp.BuildConfig.REDIRECT_URI_DESK
+import com.rcl.nextshiki.di.ktor.KtorModel
+import com.rcl.nextshiki.di.ktor.KtorRepository
 import com.rcl.nextshiki.elements.Platforms.Desktop
+import com.russhwolf.settings.Settings
+import com.russhwolf.settings.set
 import java.awt.Toolkit
 import java.awt.datatransfer.Clipboard
 import java.awt.datatransfer.StringSelection
@@ -14,4 +21,23 @@ internal actual fun copyToClipboard(text: String) {
 
 internal actual fun currentPlatform(): Platforms {
     return Desktop
+}
+
+internal actual suspend fun updateToken(ktorRepository: KtorRepository, settings: Settings) {
+    val code = settings.getStringOrNull("refCode") ?: return
+
+    val token = ktorRepository.getToken(
+        code = code,
+        clientSecret = CLIENT_SECRET_DESK,
+        clientID = CLIENT_ID_DESK,
+        redirectUri = REDIRECT_URI_DESK,
+    )
+
+    settings["refCode"] = token.refreshToken
+    if (token.accessToken != null){
+        KtorModel.token.value = token.accessToken
+    }
+    if (token.scope != null) {
+        KtorModel.scope.value = token.scope
+    }
 }
