961fd4ea49ccf2bebc608caebca65154b7374b8f
RecodeLiner <RecodeLIner@gmail.com>
2024-03-15 23:06:28 +0300

    Remove extra interfaces

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/IMain.kt
+++
@@ -1,9 +0,0 @@
-package com.rcl.nextshiki.base.main.mainpage
-
-import com.rcl.nextshiki.base.main.mainpage.subelements.CardElement
-import com.rcl.nextshiki.models.topics.HotTopics
-
-interface IMain {
-    val cardList: MutableList<CardElement>
-    val topicsList: MutableList<HotTopics>
-}
--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/newspage/NewsPageComponent.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/newspage/NewsPageComponent.kt
@@ -2,5 +2,5 @@ package com.rcl.nextshiki.base.main.newspage
 
 import com.arkivanov.decompose.ComponentContext
 
-class NewsPageComponent(context: ComponentContext) : ComponentContext by context, INewsPage {
+class NewsPageComponent(context: ComponentContext) : ComponentContext by context {
 }
