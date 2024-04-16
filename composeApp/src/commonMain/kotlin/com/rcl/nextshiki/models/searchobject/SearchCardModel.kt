5f50b9ca5e6e55d0b4ff544aed537d6309d28042
RecodeLiner <RecodeLIner@gmail.com>
2024-04-16 22:19:54 +0300

    Update search card model(id now non-nullable)

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/searchobject/SearchCardModel.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/models/searchobject/SearchCardModel.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SearchCardModel(
     @SerialName("image") val image: Image = Image(),
-    @SerialName("id") val id: Int? = null,
+    @SerialName("id") val id: Int,
     @SerialName("russian") val russian: String? = null,
     @SerialName("english") val english: String? = null,
 )
