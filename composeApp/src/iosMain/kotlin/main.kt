639a399617784f175e1bc76bd6be11b86819065a
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-04-22 17:28:48 +0300

    Update ios part

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,13 +1,26 @@
 import androidx.compose.ui.window.ComposeUIViewController
-import com.rcl.nextshiki.app
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.PredictiveBackGestureOverlay
+import com.arkivanov.essenty.backhandler.BackDispatcher
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import com.rcl.nextshiki.App
+import com.rcl.nextshiki.base.RootComponent
 import com.rcl.nextshiki.di.ktor.KtorModel.networkModule
 import com.rcl.nextshiki.di.settings.SettingsModule.settingsModule
 import com.rcl.nextshiki.setupNapier
 import org.koin.core.context.startKoin
 import platform.UIKit.UIViewController
 
+@OptIn(ExperimentalDecomposeApi::class)
 fun MainViewController(): UIViewController {
     setupNapier()
+    val context = DefaultComponentContext(LifecycleRegistry())
+    val dispatcher = context.backHandler as BackDispatcher
     startKoin { modules(networkModule,settingsModule) }
-    return ComposeUIViewController { app() }
+    return ComposeUIViewController {
+        PredictiveBackGestureOverlay(backDispatcher = dispatcher, backIcon = null) {
+            App(rootComponent = RootComponent(context))
+        }
+    }
 }
