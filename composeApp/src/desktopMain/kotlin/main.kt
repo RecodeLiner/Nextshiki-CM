502e65570b2c659a14c9949f82d1053a41fe0206
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-04-25 20:49:14 +0300

    Add rounding to desktop frame

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,6 +1,7 @@
 
 import Nextshiki.composeApp.BuildConfig.USER_AGENT
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -11,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -33,7 +35,7 @@ import org.koin.core.context.startKoin
 fun main() = application {
     setupNapier()
     startKoin {
-        modules(networkModule,settingsModule)
+        modules(networkModule, settingsModule)
     }
     val lifecycle = LifecycleRegistry()
     val windowState = rememberWindowState(width = 800.dp, height = 600.dp)
@@ -42,6 +44,7 @@ fun main() = application {
 
     LifecycleController(lifecycle, windowState)
     Window(
+        transparent = true,
         undecorated = true,
         title = USER_AGENT,
         state = windowState,
@@ -51,35 +54,39 @@ fun main() = application {
             RootComponent(DefaultComponentContext(lifecycle))
         }
 
-        App(rootComponent = root, seedColor = defaultColor) {
-            WindowDraggableArea {
-                Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
-                    Spacer(Modifier)
+        Box(modifier = Modifier.clip(RoundedCornerShape(15.dp))) {
+            App(rootComponent = root, seedColor = defaultColor,
+                topAppBar = {
+                    WindowDraggableArea {
+                        Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
+                            Spacer(Modifier)
 
-                    Row(Modifier.padding(top = 6.dp, end = 4.dp)) {
-                        IconButton(onClick = { window.isMinimized = true }) {
-                            Icon(Icons.Default.Minimize, contentDescription = "Minimize app")
-                        }
+                            Row(Modifier.padding(top = 6.dp, end = 4.dp)) {
+                                IconButton(onClick = { window.isMinimized = true }) {
+                                    Icon(Icons.Default.Minimize, contentDescription = "Minimize app")
+                                }
 
-                        IconButton(onClick = {
-                            windowState.placement = if (windowState.placement == WindowPlacement.Maximized)
-                                WindowPlacement.Floating else WindowPlacement.Maximized
-                        }) {
-                            Icon(
-                                imageVector = if (windowState.placement == WindowPlacement.Maximized) {
-                                    Icons.Default.ArrowDropDown
-                                } else {
-                                    Icons.Default.ArrowDropUp
-                                }, contentDescription = "Maximize or floating app"
-                            )
-                        }
+                                IconButton(onClick = {
+                                    windowState.placement = if (windowState.placement == WindowPlacement.Maximized)
+                                        WindowPlacement.Floating else WindowPlacement.Maximized
+                                }) {
+                                    Icon(
+                                        imageVector = if (windowState.placement == WindowPlacement.Maximized) {
+                                            Icons.Default.ArrowDropDown
+                                        } else {
+                                            Icons.Default.ArrowDropUp
+                                        }, contentDescription = "Maximize or floating app"
+                                    )
+                                }
 
-                        IconButton(onClick = ::exitApplication) {
-                            Icon(Icons.Default.Close, contentDescription = "Close app")
+                                IconButton(onClick = ::exitApplication) {
+                                    Icon(Icons.Default.Close, contentDescription = "Close app")
+                                }
+                            }
                         }
                     }
                 }
-            }
+            )
         }
     }
 }
