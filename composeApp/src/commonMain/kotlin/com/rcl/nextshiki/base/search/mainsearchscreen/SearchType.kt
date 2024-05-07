97f1a19174f585844b14e3f934f942f1612b6489
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-05-07 23:25:35 +0300

    add api path to search type

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/SearchType.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/SearchType.kt
@@ -2,11 +2,11 @@ package com.rcl.nextshiki.base.search.mainsearchscreen
 
 import com.rcl.nextshiki.locale.LocalizedString
 
-enum class SearchType(val getRes: (LocalizedString) -> String) {
-    Anime({ it.search_anime }),
-    Manga({ it.search_manga }),
-    Ranobe({ it.search_ranobe }),
-    People({ it.search_people }),
-    Users({ it.search_users }),
-    Characters({ it.search_characters })
+enum class SearchType(val getRes: (LocalizedString) -> String, val apiPath: String) {
+    Anime({ it.search_anime }, "animes"),
+    Manga({ it.search_manga }, "mangas"),
+    Ranobe({ it.search_ranobe }, "ranobe"),
+    People({ it.search_people }, "people"),
+    Users({ it.search_users }, "users"),
+    Characters({ it.search_characters }, "characters")
 }
