c82972d53bc2376fa3cb90411e9f7d952c64c1f4
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-05-10 21:34:46 +0300

    Add padding to AdaptiveRow

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/AdaptiveRow.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/AdaptiveRow.kt
@@ -1,5 +1,6 @@
 package com.rcl.nextshiki.elements
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -8,22 +9,32 @@ import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass.Companion
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun AdaptiveRow(firstRow: LazyListScope.() -> Unit, secondRow: LazyListScope.() -> Unit) {
     val widthSizeClass = calculateWindowSizeClass().widthSizeClass
-    when(widthSizeClass) {
+    when (widthSizeClass) {
         Compact -> {
-            LazyColumn {
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(5.dp)) {
                 firstRow()
                 secondRow()
             }
         }
+
         else -> {
-            Row {
-                LazyColumn(content = firstRow, modifier = Modifier.weight(1f))
-                LazyColumn(content = secondRow, modifier = Modifier.weight(1f))
+            Row(horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    content = firstRow,
+                    modifier = Modifier.weight(1f)
+                )
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    content = secondRow,
+                    modifier = Modifier.weight(1f)
+                )
             }
         }
     }
