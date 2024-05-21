e361b1f79444db7c3c641b09290a379bd82e39bb
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-05-21 23:03:13 +0300

    Remove extra char

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/newspage/NewsPageScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/newspage/NewsPageScreen.kt
@@ -1,13 +1,28 @@
 package com.rcl.nextshiki.base.main.newspage
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Error
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -87,7 +102,7 @@ fun NewsPageScreen(component: NewsPageComponent) {
                     }
                     item("${topic.id} linked") {
                         topic.linked?.let { linked ->
-                            Text("${news_linked.getLocalizableString()}}:")
+                            Text("${news_linked.getLocalizableString()}:")
                             linked.name?.let { linkedName ->
                                 Text(
                                     modifier = Modifier.padding(start = 15.dp),
