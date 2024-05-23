1a5b2506621ecc51fdc2bf6688238df56d268790
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-05-23 16:13:01 +0300

    Add russian name to card element

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/subelements/CardElement.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/main/mainpage/subelements/CardElement.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.Serializable
 data class CardElement(
     val id: Int = 0,
     val name: String = "",
+    val russian: String = "",
     val imageLink: String = "",
     val nextEpisodeAt: String = ""
 )
