bd476e663bfcaa7ec56eef7be59494a8cad96791
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-09-24 16:08:45 +0300

    Revert borders and unification of card +update libs versions

--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/MainSearchComponentScreen.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/base/search/mainsearchscreen/MainSearchComponentScreen.kt
@@ -1,6 +1,5 @@
 package com.rcl.nextshiki.base.search.mainsearchscreen
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.Orientation.Horizontal
 import androidx.compose.foundation.gestures.Orientation.Vertical
 import androidx.compose.foundation.gestures.draggable
@@ -27,7 +26,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -260,12 +258,7 @@ private fun TypeCard(
     Card(
         modifier = Modifier
             .padding(horizontal = 2.dp)
-            .noRippleClickable(onClick)
-            .border(
-                color = colorScheme.onBackground,
-                width = 1.dp,
-                shape = CardDefaults.shape
-            ),
+            .noRippleClickable(onClick),
         colors =
         if (selected)
             getSelectedCardColor(colorScheme)
--- composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/SearchCardObject.kt
+++ composeApp/src/commonMain/kotlin/com/rcl/nextshiki/elements/SearchCardObject.kt
@@ -1,7 +1,6 @@
 package com.rcl.nextshiki.elements
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,14 +8,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -29,11 +27,6 @@ fun SearchCard(
         modifier = Modifier
             .fillMaxSize()
             .then(modifier)
-            .border(
-                color = MaterialTheme.colorScheme.onBackground,
-                width = 1.dp,
-                shape = CardDefaults.shape
-            )
     ) {
         Column {
             Image(
@@ -44,15 +37,13 @@ fun SearchCard(
                     .fillMaxWidth()
                     .aspectRatio(1f)
                     .clip(RoundedCornerShape(15.dp))
-                    .border(
-                        color = MaterialTheme.colorScheme.onBackground,
-                        width = 1.dp,
-                        shape = RoundedCornerShape(15.dp)
-                    )
             )
             Text(
                 text = name,
                 softWrap = true,
+                maxLines = 2,
+                minLines = 2,
+                overflow = TextOverflow.Ellipsis,
                 modifier = Modifier.padding(12.dp)
             )
         }
